d4092d65540c91e9e21936bb79233b70d8fbcf95
behike56 <engineering@lambda-publishing.dev>
2024-09-26 22:31:15 +0900

    tiny fix.

--- DML/在職者＆従業員.sql
+++ DML/在職者＆従業員.sql
@@ -15,3 +15,21 @@ FROM employee emp
       )
       AND ih1.issue_type != '06'
   ) tenure ON emp.employee_id = tenure.employee_id;
+
+SELECT emp.employee_id,
+  tenure.issue_type,
+  tenure.issue_date
+FROM employee emp
+  JOIN (
+    SELECT ih1.employee_id,
+      ih1.issue_type,
+      ih1.issue_date
+    FROM issue_history ih1
+    WHERE ih1.issue_date EXISTS (
+        SELECT MAX(ih2.issue_date)
+        FROM issue_history ih2
+          JOIN system_date sysd ON ih2.issue_date <= sysd.sys_date
+          AND ih1.employee_id = ih2.employee_id
+      )
+      AND ih1.issue_type != '06'
+  ) tenure ON emp.employee_id = tenure.employee_id;
